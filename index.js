1318a46a2272d6fe4decf0c61771d1b2f34858fa
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-06-05 14:21:06 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,141 @@
+// 1chi masala
+// let arr = [];
+// for (let i = 1; i <= 5; i++) {
+//   arr.push(1);
+//   console.log(arr);
+// }
+
+
+// 2chi masala
+// let arr = [];
+// arr.push(1,2,3);
+// arr.pop();
+// arr.toString;
+// console.log(arr);
+
+
+// 3chi masala
+// let arr = [];
+// arr.unshift(1,2,3,4);
+// arr.reverse();
+// console.log(arr,'uzunlik->',arr.length);
+
+
+// 4chi masala
+// let arr = [];
+// arr.unshift(1,2,3);
+// arr.shift(0);
+// arr.join()
+// console.log(arr);
+
+
+// 5chi masala
+// let arr = [3,true, 4, 'true'];
+// arr.pop();
+// console.log(arr.length,  arr, arr.at(0));
+
+
+// 6chi masala
+// let arr = [1,false,'true',4];
+// arr.push('sss');
+// arr.toString()
+// console.log(typeof arr.at(-1));
+
+
+// 7chi masala
+// let arr = [1,2,3,4]
+// arr.unshift(11);
+// arr.reverse();
+// console.log(arr, arr.length);
+
+
+// 8chi chmasala
+// let arr = [1,2,3,4];
+// arr.shift(0);
+// let a = arr.join()
+// console.log(a, arr.length);
+
+
+// 9chi masala
+// let arr = [1,2,3,4];
+// arr.push(9,32);
+// arr.unshift(12);
+// let u = arr.length;
+// let t = arr.reverse();
+// let string = arr.join();
+// console.log(t, string, u);
+
+
+// 10chi masala
+// let arr = [1,2,3,4,5,6]
+// let a = arr.pop();
+// let b = arr.shift();
+// let u = arr.length;
+// let t = arr.reverse();
+// console.log(t, u);
+
+// 11chi masala
+// let arr = [1,2,3,4,5]
+// let t = arr.reverse();
+// let string = arr.toString();
+// let u = string.length;
+// console.log(t, string , u);
+
+
+// 12chi masala
+//  let arr = [1,2,3,4,5]
+//  let arr1 = [6,7,8,9]
+//  let q = arr.concat(arr1);
+//  let o = q.slice(1,-1);
+//  let u = q.length;
+//  console.log(q, u);
+
+
+// 13chi masala
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let o = arr.splice(2 , 3);
+// let string = o.toString();
+// let u = string.length;
+// console.log(o , string , u);
+
+
+// 14chi masala
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let k = arr.slice(3, -1);
+// let t = k.reverse();
+// console.log(t);
+
+// 15chi masala
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let flat = arr.flat();
+// let o = flat.at(-1);
+// console.log(o);
+
+
+// 17chi masala
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let t = arr.reverse();
+// let string = arr.join();
+// let o= string.at(-1);
+// console.log(t , string , o);
+
+
+// 18-masala
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let a = arr.slice(0,2);
+// let b= arr.splice(4,8);
+// console.log(b);
+
+
+// 19chi masala
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let k = arr.splice(0,2);
+// let t = arr.reverse();
+// console.log(t);
+
+
+// 20chi masala
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let a = arr.a();
+// let b = a.b()
+// console.log(a , b );
